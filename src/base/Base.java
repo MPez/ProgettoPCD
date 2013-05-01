0e8809a97c15796d3b0ff40a5d9fe642386512da
Marco <marco.pezzutti@hotmail.it>
2013-05-01 23:20:46 +0200

    incremento base; implementato ditta

---
+++ src/base/Base.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base;
+
+import common.IAutotreno;
+import common.IBase;
+import java.rmi.RemoteException;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author marco
+ */
+public class Base implements IBase {
+    private String nomeBase;
+    private LinkedList<IBase> listaDestinazioni;
+    private LinkedList<IAutotreno> listaAutotreni;
+    private LinkedList<IBase> basiAttive;
+    private LinkedList<IAutotreno> autotreniAttivi;
+    
+    private BaseGUI gui;
+
+    Base(String nomeBase, BaseGUI gui) {
+        this.nomeBase = nomeBase;
+        this.gui = gui;
+        
+    }
+    
+    @Override
+    public String getNomeBase() {
+        return nomeBase;
+    }
+
+    @Override
+    public synchronized void registraOrdine(IBase destinazione) {
+        listaDestinazioni.add(destinazione);
+    }
+
+    @Override
+    public void riceviMerce(IAutotreno autotreno) {
+        parcheggia(autotreno);
+    }
+
+    @Override
+    public boolean stato()  {
+        return true;
+    }
+
+    @Override
+    public void terminaAttività() {
+        
+    }
+    
+    private synchronized void parcheggia(IAutotreno autotreno) {
+        listaAutotreni.add(autotreno);
+    }
+    
+    private class InviaOrdine implements Runnable {
+        private IBase destinazione;
+        private IAutotreno autotreno;
+        
+        @Override
+        public void run() {
+            
+        }
+    }
+    
+}
